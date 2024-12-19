c81245be549c6d119dc6a431f4f6bb1226758376
Marius <mariusfagerland@yahoo.no>
2024-12-19 17:03:49 +0000

    Add telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,37 @@
+local function config()
+	-- -- Enable Telescope extensions if they are installed
+	 pcall(require('telescope').load_extension, 'fzf')
+	 pcall(require('telescope').load_extension, 'ui-select')
+
+  local builtin = require('telescope.builtin')
+  vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Search Files'})
+  vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Search Grep'})
+  vim.keymap.set('n', '<leader>,', builtin.buffers, { desc = 'Search Buffers'})
+end
+
+return {
+	"nvim-telescope/telescope.nvim",
+	version = "0.1.8",
+	event = "VimEnter",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
+        'nvim-telescope/telescope-fzf-native.nvim',
+
+        -- `build` is used to run some command when the plugin is installed/updated.
+        -- This is only run then, not every time Neovim starts up.
+        build = 'make',
+
+        -- `cond` is a condition used to determine whether this plugin should be
+        -- installed and loaded.
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+
+      -- Useful for getting pretty icons, but requires a Nerd Font.
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+	},
+	config = config,
+}
