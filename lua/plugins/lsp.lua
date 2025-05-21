77100d8e621dcb91bed9a91ea24eecaf33aabda9
Marius <mariusfagerland@yahoo.no>
2025-05-21 10:29:25 +0000

    fix nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,9 +1,10 @@
 return {
 	"neovim/nvim-lspconfig",
-	version = "v1.1.0",
+	version = "v2.1.0",
 	dependencies = {
 		{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
 		"williamboman/mason-lspconfig.nvim",
+
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 
 		{ "j-hui/fidget.nvim", opts = {} },
